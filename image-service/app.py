ad74ee31efb82dc15905f1d0d6af8194db06509c
Lucas Oliveira da Silva <lucasooliveiranh@gmail.com>
2024-09-22 21:56:36 -0300

    updates complex images

--- image-service/app.py
+++ image-service/app.py
@@ -1,7 +1,7 @@
 # image-service/app.py
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from image_generator import generate_image
+from image_generator import generate_high_resolution_image, generate_complex_pattern_image, generate_generative_art, apply_image_filter, generate_with_additional_elements, batch_process_images
 import pika
 
 app = Flask(__name__)
@@ -26,17 +26,30 @@ def publish_message(image_path):
 @app.route('/generate', methods=['POST'])
 def generate():
     data = request.json
-    text = data.get('text', 'Hello World123')
-    num_images = data.get('numImages', 1)  # Get the number of images from the request
+    text = data.get('text', 'Hello World')
+    num_images = data.get('numImages', 1)
+    image_type = data.get('type')  # Get the selected type from the request
+
     image_paths = []
 
     for i in range(int(num_images)):
-        image_path = generate_image(text, i)  # Pass index to make unique file names
+        if image_type == 'high_resolution':
+            image_path = generate_high_resolution_image(text, i)
+        elif image_type == 'complex_patterns':
+            image_path = generate_complex_pattern_image(text, i)
+        elif image_type == 'generative_art':
+            image_path = generate_generative_art(text, i)
+        elif image_type == 'image_filters':
+            image_path = apply_image_filter(text, i)
+        elif image_type == 'additional_elements':
+            image_path = generate_with_additional_elements(text, i)
+        elif image_type == 'batch_processing':
+            image_path = batch_process_images(text, i)
+        else:
+            image_path = generate_high_resolution_image(text, i)  # Default option
+
         image_paths.append(image_path)
 
-        # Publish the image path to the queue for the db-service to save
-        publish_message(image_path)
-        
     return jsonify({"image_paths": image_paths})
 
 if __name__ == '__main__':
